7d66625e3a8717d12e80933972c17fcb1026d493
azucenareyes <sofia878@gmail.com>
2019-05-27 15:31:35 -0600

    created my database with two Schemas

--- db/index.js
+++ db/index.js
@@ -1,9 +1,46 @@
-// bringing in mongoose
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/meta-data', {useNewUrlParser: true});
 
 
-//connecting to database 
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 
+
+var Schema = mongoose.Schema;
+
+  var singlePlayerGame = new Schema({
+    title:  String,
+    image: String,
+    type: String,
+    body:   String,
+    meta: {
+      votes: Number,
+      favs:  Number
+    }
+  });
+
+  
+  var multiPlayerGame = new Schema({
+    title:  String,
+    type: String,
+    body:   String,
+    meta: {
+      votes: Number,
+      favs:  Number
+    }
+  });
+  
+ var SinglePlayer = mongoose.model('SinglePlayer', singlePlayerGame);
+ var MultiPlayer = mongoose.model('Multiplayer', multiPlayerGame);
  
+ 
+ let SinglePlayer = SinglePlayer.findOne({ 'type' : 'singlePlayer'}, 'image title', function(err, gameFound){
+   if(err) return handleError(err);
+   //would like to return: gameFound.image and gameFound.title here...
+   return gameFound ;
+ })
+ 
+module.exports.SinglePlayer = SinglePlayer;
+module.exports.MultiPlayer = MultiPlayer;
+
