c6ebfe71cb20939f93df1d20af3c0440fb264ebc
azucenareyes <sofia878@gmail.com>
2019-05-29 17:47:47 -0600

    added the proper get route and got the API get endpoint working

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
-const db = require('../db/index.js')
-const seeder = require('../db/gameSeeder.js')
+const LanguageFeatures = require('../db/index')
 const express = require('express');  
 const app = express(); 
 
@@ -9,14 +8,17 @@ const bodyParser = require('body-parser');
 
 
 app.use(express.static(__dirname + '../db/index.js'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-
-app.get('/meta-data', function (req, res) {
-  res.send('Hello World');
-})
+app.get('/:id', (req, res) => {
+  let requestedData = req.params.id;
+  LanguageFeatures.find({ id: requestedData}, (err, result)=> {
+    res.send(result[0]);
+  });
+});
 
 
 app.listen(port, function(){
