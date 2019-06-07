10fcd7cd643916b5432386c80a2b58de8b4f1e70
glofishh <gloriaulee@gmail.com>
2019-06-06 23:06:58 -0400

    generating data using faker. next: generate 10mil of them

--- db/PGseeder.js
+++ db/PGseeder.js
@@ -1,1 +1,37 @@
 const faker = require('faker');
+const fs = require('fs');
+
+
+let count = 0;
+const product = {};
+product.id = count;
+product.English = { interface: faker.random.boolean(), 
+                    full_audio: faker.random.boolean(),
+                    subtitles: faker.random.boolean(),
+                  };
+product.Spanish = { interface: faker.random.boolean(), 
+                    full_audio: faker.random.boolean(),
+                    subtitles: faker.random.boolean(),
+                  };
+product.French = { interface: faker.random.boolean(), 
+                    full_audio: faker.random.boolean(),
+                    subtitles: faker.random.boolean(),
+                  };
+product.Navajo = { interface: faker.random.boolean(), 
+                    full_audio: faker.random.boolean(),
+                    subtitles: faker.random.boolean(),
+                  };
+product.Mandarin = { interface: faker.random.boolean(), 
+                    full_audio: faker.random.boolean(),
+                    subtitles: faker.random.boolean(),
+                  };
+count++;
+
+const testingThingy = () => {
+  let thingy = 0;
+  while (thingy < 10) {
+    console.log(faker.random.boolean());
+  thingy++;
+  }
+}
+testingThingy();
